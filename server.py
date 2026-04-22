f413b4c7010fc81af4c91e8d682a69520fc0138e
chenstephen929-maker <chenstephen929@gmail.com>
2026-04-22 09:17:41 +0800

    handle clients adding

--- server.py
+++ server.py
@@ -7,11 +7,55 @@ class server:
     id=0
     port=55555
     display_interval=10
+    tuple={}
+    total_clients=0
+    read_num=0
+    get_num=0
+    put_num=0
+    error_num=0
+    operation_num=0
 
     def __init__(self,hostname,id):
         self.hostname=hostname
         self.myserver=[]
         self.id=id
+
+    def handle_clients(self,client_socket,addr):
+        print(f"connected from {addr}...")
+        try:
+            message=client_socket.recv(1024).decode('utf-8')
+            message_split=message.split()
+            action=message_split(0)
+            key=message_split(1)
+            value=message_split(2)
+            handle_request()
+        except Exception as ex:
+            print(f"Error handling client {addr}")
+            self.error_num+=1
+        finally:
+            client_socket.close()
+            print(f"connection with {addr} closed")
+
+        def handle_request(self,message):
+            global tuple
+            part=message.split(' ')
+            size=part[0]
+            operation=part[1]
+            key=part[2]
+            dict_tuple=dict(self.tuple)
+
+            match operation:
+                case 'R':
+                    if(dict_tuple[f'{key}']!=None):
+                        response=f"{size} OK ({key}{dict_tuple[f'{key}']} read)"
+                        client_socket.sendall(response.encode('utf-8'))
+                    else:
+                        self.error_num+=1
+                        response=f"{size} ERR {key} does not exist"
+                case 'G':
+
+
+
         
     
     def start_server(self):
